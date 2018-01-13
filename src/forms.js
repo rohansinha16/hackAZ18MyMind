c2d360c6c411bfdf1f30fb4b0a7093a84fe9b44b
Sydni Quincey <35391013+SQuincey@users.noreply.github.com>
2018-01-13 01:28:48 -0700

    Update forms.js

--- src/forms.js
+++ src/forms.js
@@ -7,7 +7,8 @@ function depression(){
 			" respond to these questions with a 0, 1, 2, 3, or 4; 0 meaning not at all or" +
 			" less than 1 day and 4 being nearly every day for 2 weeks. Lets begin.",
 		outrto: "Your entry has been logged. This scale was built using" +
-			" the Center for Epidemiologic Studies Depression Scale – Revised (CESD-R)",
+			" the Center for Epidemiologic Studies Depression Scale – Revised (CESD-R)." +
+			" Please visit http://cesd-r.com/ for more information.",
 		help: "To answer these questions, please say a number between 0 and 4." +
 			" 0 indicates not at all or less than 1 day. 1 indicates 1 to 2 days." +
 			" 2 indicates 3 to 4 days. 3 indicates 5 to 7 days. And 4 indicates nearly" +
@@ -38,25 +39,4 @@ function depression(){
 		]
 	}
 }
-function diagnosisDep(questions){
-	if(questions[7] == 4 && questions[9] == 4){
-		var countFour = 0;
-		var countThree = 0;
-		for(var i = 0; i < questions.length; i++){
-			if(i == 7 || i == 9){
-				continue;
-			}
-			if(questions[i] == 4){
-				countFour++;
-			}
-			else if(){
-			}
-		}
-	}
-	else if(questions[1] == 4 && questions[3] == 4 && questions[5] == 4){
-		
-	}
-	else{
-	
-	}
-}
+
