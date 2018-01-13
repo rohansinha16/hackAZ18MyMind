32666dc279c557a2b7d3107df64e14a9644a1ef0
Sydni Quincey <35391013+SQuincey@users.noreply.github.com>
2018-01-13 00:01:49 -0700

    Update forms.js

--- src/forms.js
+++ src/forms.js
@@ -2,6 +2,16 @@ function depression(){
 	return {
 		min: 0,
 		max: 4,
+		intro: "In this Depression Scale, I will be asking you 20 questions on" +
+		"the ways you may have felt or behaved over the past week or so. Please" +
+		"respond to these questions with a 0, 1, 2, 3, or 4; 0 meaning not at all or" +
+		"less than 1 day and 4 being nearly every day for 2 weeks. Lets begin.",
+		outrto: "Your entry has been logged. This scale was built using" +
+		"the Center for Epidemiologic Studies Depression Scale – Revised (CESD-R)",
+		help: "To answer these questions, please say a number between 0 and 4." +
+		"0 indicates not at all or less than 1 day. 1 indicates 1 to 2 days." +
+		"2 indicates 3 to 4 days. 3 indicates 5 to 7 days. And 4 indicates nearly" +
+		"everyday for 2 weeks. Lets resume. ",
 		goal: "lower",
 		questions: [ 
 			"My appetite was poor",
