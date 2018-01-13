8784840af874da9198875141f50801526de6113a
Sydni Quincey <35391013+SQuincey@users.noreply.github.com>
2018-01-12 23:29:30 -0700

    Update forms.js

--- src/forms.js
+++ src/forms.js
@@ -1,12 +1,31 @@
-function anxiety(){
+function depression(){
 	return {
 		min: 0,
-		max: 3,
+		max: 4,
 		goal: "lower",
-		questions: [
-			"Question 1",
-			"Question 2",
+		questions: [ 
+			"My appetite was poor",
+			"I could not shake off the blues",
+			"I had trouble keeping my mind on what I was doing",
+			"I felt depressed",
+			"My sleep was restless",
+			"I felt sad",
+			"My sleep was restless",
+			"I could not get going",
+			"Nothing made me happy",
+			"I felt like a bad person",
+			"I lost interest in my usual activities",
+			"I slept much more than usual",
+			"I felt like I was moving too slowly",
+			"I felt fidgety",
+			"I wished I were dead",
+			"I wanted to hurt myself",
+			"I was tired all the time",
+			"I did not like myself",
+			"I lost a lot of weight without trying to",
+			"I had a lot of trouble getting to sleep",
+			"I could not focus on the important things",
 		]
-	};
+	}
 }
 
